e8d36f0c8a69064cd035776188d0523ad9f38bc2
Ivan-Krul <kiwiivanya128@gmail.com>
2024-05-07 22:09:26 +0200

    made a dummy bot, but it requires c++17

--- DiscordPuppeteer/src/main.cpp
+++ DiscordPuppeteer/src/main.cpp
@@ -2,7 +2,34 @@
 
 #include "EnviroumentFile.h"
 
+#include "dpp/dpp.h"
+
 int main() {
     EnviroumentFile env;
-    std::cout << "Hi\n";
+    std::cout << env["CLIENTID"] << "\n";
+
+    dpp::cluster bot(env["TOKEN"]);
+
+    bot.on_log(dpp::utility::cout_logger());
+
+    bot.on_ready([&bot](const dpp::ready_t& event) {
+        if (dpp::run_once<struct register_bot_commands>()) {
+            std::vector<dpp::slashcommand> commands{
+                {"ping", "pong!", bot.me.id}
+            };
+            bot.global_bulk_command_create(commands);
+        }
+    });
+
+    bot.on_slashcommand([](const dpp::slashcommand_t& event) {
+        if (event.command.get_command_name() == "ping") {
+            std::cout << "pong!\n";
+            event.reply("pong!");
+            
+        }
+        //return dpp::task_dummy();
+    });
+
+    bot.start(dpp::st_wait);
+
 }
